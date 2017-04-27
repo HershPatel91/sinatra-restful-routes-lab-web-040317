1ef2bc031c62828c47f7959e817d7f588fb62065
github username <github email address>
2017-04-27 10:23:28 -0400

    Done.

---
+++ db/migrate/20170426222809_recipes.rb
@@ -0,0 +1,10 @@
+class Recipes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+    end
+  end
+end
+
